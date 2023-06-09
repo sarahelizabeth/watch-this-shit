52fb180b1b2cf19542de410bb894a0fda1cdece8
sarahelizabeth <semurray3@gmail.com>
2023-06-09 02:29:59 -0500

    added profile_list template and view

--- watchthisshit/models.py
+++ watchthisshit/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField( 
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.user.username
+    
+# Note: Django documentation mentions that the best place 
+# to put your signals is in a new signals.py submodule of 
+# your app. However, this requires you to make additional 
+# changes in your app configuration. Since you only need 
+# to build out a single signal for this tutorial, you’re 
+# keeping it in models.py.
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
--- watchthisshit/views.py
+++ watchthisshit/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def dashboard(request):
+    return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, 'watchthisshit/profile_list.html', {'profiles': profiles})
